50dd3ed401d984f858eb26776f8532318ca50d3e
Lars Bahner <lars.bahner@gmail.com>
2023-08-09 05:01:56 +0200

    Add mdns discovery.

--- discovery.go
+++ discovery.go
@@ -2,63 +2,23 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	"github.com/libp2p/go-libp2p/core/host"
-	// "github.com/libp2p/go-libp2p/p2p/discovery/mdns"
-	"github.com/libp2p/go-libp2p/p2p/discovery"
-	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
-	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
-	"github.com/libp2p/p2p/discovery/mdns"
+	log "github.com/sirupsen/logrus"
 )
 
-// discoverPeers performs peer discovery using the DHT and connects to discovered peers
-func discoverPeers(ctx context.Context, h host.Host, rendezvousString string) error {
-	dhtInstance, err := initDHT(ctx, h)
-	if err != nil {
-		return err
-	}
+func discoverPeers(ctx context.Context, h host.Host, rendezvous string) {
 
-	// Set up an mDNS service on the libp2p host
-	// ser, err := mdns.NewMdnsService(ctx, h, time.Second*5, rendezvousString)
-	ser := mdns.NewMdnsService(ctx, h, time.Second*5, rendezvousString)
-	ser.RegisterNotifee(&discovery.Notifee{})
-	// if err != nil {
-	// 	panic(err)
-	// }
+	log.Info("libp2p node created: ", h.ID().Pretty(), " ", h.Addrs())
 
-	// The service will run in the background printing discovered peers to the console
-	// ser.RegisterNotifee(&discovery.Notifee{})
+	// Start peer discovery to find other peers
+	log.Debug("Starting peer discovery...")
 
-	routingDiscovery := drouting.NewRoutingDiscovery(dhtInstance)
-	dutil.Advertise(ctx, routingDiscovery, rendezvousString)
+	go discoverDHTPeers(ctx, h, rendezvous)
+	go discoverMDNSPeers(ctx, h, rendezvous)
 
-	// Look for others who have announced and attempt to connect to them
-	anyConnected := false
-	for !anyConnected {
-		logger.Info("Searching for peers...")
-		peerChan, err := routingDiscovery.FindPeers(ctx, rendezvousString)
-		if err != nil {
-			return fmt.Errorf("peer discovery error: %w", err)
-		}
+	log.Info("Peer discovery completed.")
 
-		for peer := range peerChan {
-			if peer.ID == h.ID() {
-				continue // Skip self connection
-			}
+	select {}
 
-			err := h.Connect(ctx, peer)
-			if err != nil {
-				logger.Errorf("Failed connecting to %s, error: %v\n", peer.ID.Pretty(), err)
-			} else {
-				fmt.Println("Connected to:", peer.ID.Pretty())
-				anyConnected = true
-			}
-		}
-	}
-
-	logger.Info("Peer discovery complete")
-
-	return nil
 }
--- go.mod
+++ go.mod
@@ -7,9 +7,10 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/gorilla/websocket v1.5.0
 	github.com/ipfs/go-log v1.0.5
-	github.com/libp2p/go-libp2p v0.29.0
-	github.com/libp2p/go-libp2p-kad-dht v0.24.2
+	github.com/libp2p/go-libp2p v0.29.2
+	github.com/libp2p/go-libp2p-kad-dht v0.24.3
 	github.com/libp2p/go-libp2p-pubsub v0.9.3
+	github.com/sirupsen/logrus v1.9.3
 	go.deanishe.net/env v0.5.1
 )
 
@@ -72,6 +73,7 @@ require (
 	github.com/libp2p/go-netroute v0.2.1 // indirect
 	github.com/libp2p/go-reuseport v0.3.0 // indirect
 	github.com/libp2p/go-yamux/v4 v4.0.1 // indirect
+	github.com/libp2p/zeroconf/v2 v2.2.0 // indirect
 	github.com/marten-seemann/tcp v0.0.0-20210406111302-dfbc87cc63fd // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
@@ -104,9 +106,9 @@ require (
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
 	github.com/quic-go/qpack v0.4.0 // indirect
-	github.com/quic-go/qtls-go1-19 v0.3.2 // indirect
-	github.com/quic-go/qtls-go1-20 v0.2.2 // indirect
-	github.com/quic-go/quic-go v0.36.2 // indirect
+	github.com/quic-go/qtls-go1-19 v0.3.3 // indirect
+	github.com/quic-go/qtls-go1-20 v0.2.3 // indirect
+	github.com/quic-go/quic-go v0.36.4 // indirect
 	github.com/quic-go/webtransport-go v0.5.3 // indirect
 	github.com/raulk/go-watchdog v1.3.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
